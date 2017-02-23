e5f42b8265b40a926c01c87adf9082ccb6ccdc24
Bahadir Can Yildiz <bahadircyildiz@yandex.com>
2017-02-23 10:53:10 -0500

    index.php error

--- www/ExcelParse/index.php
+++ www/ExcelParse/index.php
@@ -6,23 +6,25 @@ date_default_timezone_set('Europe/London');
 
 define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
 
+
+//Excel File constants
+
+define("USERINFO_LAST_COL", 7);
+
 // /** Include PHPExcel */
 require_once dirname(__FILE__) . '/Classes/PHPExcel.php';
 
-var_dump($_FILES);
+$response = [];
 
 if(isset($_FILES['spreadsheet'])){
-    echo "Found Spreadsheet file";
     if($_FILES['spreadsheet']['tmp_name']){
         if(!$_FILES['spreadsheet']['error']){
-            echo "No Errors on Spreadsheet file";
             $inputFile = $_FILES['spreadsheet']['tmp_name'];
             $extension = strtoupper(pathinfo($_FILES['spreadsheet']['name'], PATHINFO_EXTENSION));
-            echo "File extension " . $extension . " Input File" . $inputFiles;
             if($extension == 'XLSX' || $extension == 'ODS'){
                 //Read spreadsheeet workbook
                 try {
-                    echo "Reading Spreadsheet file";
+                    
                     $inputFileType = PHPExcel_IOFactory::identify($inputFile);
                     $objReader = PHPExcel_IOFactory::createReader($inputFileType);
                     $objPHPExcel = $objReader->load($inputFile);
@@ -33,14 +35,20 @@ if(isset($_FILES['spreadsheet'])){
                 $sheet = $objPHPExcel->getSheet(0); 
                 $highestRow = $sheet->getHighestRow(); 
                 $highestColumn = $sheet->getHighestColumn();
-        
+                
+                $attributes = $sheet->rangeToArray('A1:' . $highestColumn . "1", NULL, TRUE, FALSE)
                 //Loop through each row of the worksheet in turn
-                for ($row = 1; $row <= $highestRow; $row++){ 
+                for ($row = 2; $row <= $highestRow; $row++){ 
                         //  Read a row of data into an array
-                        $rowData = $sheet->rangeToArray('A' . $row . ':' . $highestColumn . $row, NULL, TRUE, FALSE);
+                        $rowData = $sheet->rangeToArray('A' . $row . ':' . USERINFO_LAST_COL . $row, NULL, TRUE, FALSE);
+                        $person = [];
+                        for ($attrow = 0; $attrow <= USERINFO_LAST_COL; $attrow++){
+                            $person[$attributes[$attrow]] = $rowData[$attrow];
+                        }
+                        array_push($response, $person);                        
                         //Insert into database
-                        var_dump($rowData);
                 }
+                var_dump($response);
             }
             else{
                 echo "Please upload an XLSX or ODS file";
