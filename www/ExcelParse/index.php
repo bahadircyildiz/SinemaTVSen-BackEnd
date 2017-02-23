24a34676eae89f949ff6362c7a970ca91348ca78
Bahadir Can Yildiz <bahadircyildiz@yandex.com>
2017-02-23 07:50:52 -0500

    changed ExcelParse

---
+++ www/ExcelParse/index.php
@@ -0,0 +1,16 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', TRUE); 
+ini_set('display_startup_errors', TRUE); 
+date_default_timezone_set('Europe/London');
+
+define('EOL',(PHP_SAPI == 'cli') ? PHP_EOL : '<br />');
+
+// /** Include PHPExcel */
+require_once dirname(__FILE__) . '/../Classes/PHPExcel.php';
+
+$debug = var_export($_POST, true);
+
+echo $debug;
+
+?>
