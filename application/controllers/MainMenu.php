da9a6e2afa64308696c332c8d2c8315569257624
Bahadir Can Yildiz <bahadircyildiz@yandex.com>
2017-11-26 18:26:43 +0300

    trying to solve enoent index.loxk error

--- application/controllers/MainMenu.php
+++ application/controllers/MainMenu.php
@@ -3,16 +3,22 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class MainMenu extends MY_Controller {
 
-    public function index()
+    function __construct(){
+        parent::__construct();
+    }
+
+    function index()
     {
-        $this->title = "Welcome!!!";
-        $this->load->view('options_view');
+        $this->parse_excel();
     }
 
     function parse_excel()
     {
-        $params = $_FILES['spreadsheet'];
-        $data = $this->ExcelHandler_model->parse_excel($params);
+        $data = array();
+        if($_FILES){
+            $params = $_FILES['spreadsheet'];
+            $data = $this->ExcelHandler_model->parse_excel($params);
+        }
         $this->load->view("options_view", array( 'data' => $data));
     }
 
